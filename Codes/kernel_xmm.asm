62f158d29dc2d191c2132ee3ec7902be415fe2bb
laureana-000 <119325983+laureana-000@users.noreply.github.com>
2026-03-11 23:25:13 +0800

    Add files via upload
    updated codes for xmm and ymm (with boundaries)

--- Codes/kernel_xmm.asm
+++ Codes/kernel_xmm.asm
@@ -1,29 +1,70 @@
 section .text
 global kernel_xmm
 
+; rcx = n
+; rdx = A
+; r8  = B
+; r9  = C
+; idx = [rsp+40]
+
 kernel_xmm:
 
-    mov r10, rcx    ; n
-    mov r11, rdx    ; A
-    mov r12, r8     ; B
-    mov r13, r9     ; C
+    push rbp
+    mov rbp, rsp
 
-    xor r14, r14
+    mov r10, rcx        ; n
+    mov r11, rdx        ; A
+    mov r12, r8         ; B
+    mov r13, r9         ; C
+    mov r14, [rbp+48]   ; idx pointer
 
-loop_simd:
+    xor r15, r15        ; i = 0
 
-    cmp r14, r10
-    jge done
+; ----------------------
+; SIMD LOOP (4 floats)
+; ----------------------
+simd_loop:
 
-    vmovups xmm0, [r11 + r14*4]
-    vmovups xmm1, [r12 + r14*4]
+    mov rax, r10
+    sub rax, 4
+    cmp r15, rax
+    jg scalar_loop
+
+    vmovups xmm0, [r11 + r15*4]
+    vmovups xmm1, [r12 + r15*4]
 
     vmaxps xmm2, xmm0, xmm1
 
-    vmovups [r13 + r14*4], xmm2
+    vmovups [r13 + r15*4], xmm2
 
-    add r14, 4
-    jmp loop_simd
+    add r15, 4
+    jmp simd_loop
+
+; REMAINDER LOOP
+scalar_loop:
+
+    cmp r15, r10
+    jge done
+
+    movss xmm0, [r11 + r15*4]
+    movss xmm1, [r12 + r15*4]
+
+    comiss xmm0, xmm1
+    jb b_larger
+
+a_larger:
+    movss [r13 + r15*4], xmm0
+    mov dword [r14 + r15*4], 0
+    jmp next
+
+b_larger:
+    movss [r13 + r15*4], xmm1
+    mov dword [r14 + r15*4], 1
+
+next:
+    inc r15
+    jmp scalar_loop
 
 done:
+    pop rbp
     ret
--- Codes/kernel_ymm.asm
+++ Codes/kernel_ymm.asm
@@ -3,28 +3,63 @@ global kernel_ymm
 
 kernel_ymm:
 
+    push rbp
+    mov rbp, rsp
+
     mov r10, rcx
     mov r11, rdx
     mov r12, r8
     mov r13, r9
+    mov r14, [rbp+48]
 
-    xor r14, r14
+    xor r15, r15
 
-loop_ymm:
+; ----------------------
+; SIMD LOOP (8 floats)
+; ----------------------
+simd_loop:
 
-    cmp r14, r10
-    jge done
+    mov rax, r10
+    sub rax, 8
+    cmp r15, rax
+    jg scalar_loop
 
-    vmovups ymm0, [r11 + r14*4]
-    vmovups ymm1, [r12 + r14*4]
+    vmovups ymm0, [r11 + r15*4]
+    vmovups ymm1, [r12 + r15*4]
 
     vmaxps ymm2, ymm0, ymm1
 
-    vmovups [r13 + r14*4], ymm2
+    vmovups [r13 + r15*4], ymm2
 
-    add r14, 8
-    jmp loop_ymm
+    add r15, 8
+    jmp simd_loop
+
+; REMAINDER LOOP
+scalar_loop:
+
+    cmp r15, r10
+    jge done
+
+    movss xmm0, [r11 + r15*4]
+    movss xmm1, [r12 + r15*4]
+
+    comiss xmm0, xmm1
+    jb b_larger
+
+a_larger:
+    movss [r13 + r15*4], xmm0
+    mov dword [r14 + r15*4], 0
+    jmp next
+
+b_larger:
+    movss [r13 + r15*4], xmm1
+    mov dword [r14 + r15*4], 1
+
+next:
+    inc r15
+    jmp scalar_loop
 
 done:
     vzeroupper
+    pop rbp
     ret
